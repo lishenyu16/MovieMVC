8aa87c0450673a2fdb3c857e24c31de2937ac0f5
lishenyu16 <lishenyu16@gmail.com>
2024-05-31 17:30:20 -0700

    Add project files.

---
+++ MovieMVC/Models/Movie.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MovieMVC.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        [Display(Name = "Release Date")]
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+
+        [Display(Name = "Movie Price")]
+        [DisplayFormat(DataFormatString = "{0:C}")]
+        public decimal Price { get; set; }
+    }
+
+    public class MovieDbContext: DbContext
+    {
+        public DbSet<Movie> Movies { get; set;}
+    }
+}
